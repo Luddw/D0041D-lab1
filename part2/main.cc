eed8e969712037c3873250215251ffb6c7b62ab8
Ludwig Lindmark <carlaxelludwig@hotmail.com>
2021-04-20 19:14:03 +0200

    check for spaces

--- part2/main.cc
+++ part2/main.cc
@@ -1,20 +1,56 @@
+#include <algorithm>
+#include <string>
+#include <cctype>
 #include <chrono>
 #include <iostream>
-#include "string.h"
 #include <stack>
 
+bool IsPalindrome(std::string input){
 
-bool IsPalindrome(const std::string &input){
+    std::stack<char> stack1;
+    std::stack<char> stack2;
+    int last_char = input.size() - 1;
 
-    std::stack<int> s;
-    return false;
+    std::cout << "before : " << input << std::endl;
+    std::cout << "after : " << input << std::endl;
+    
+
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        stack1.push(input[i]);
+        std::cout << input[last_char - i] << std::endl;
+        stack2.push(input[last_char - i]);
+    }
+
+    while (!stack1.empty() && !stack2.empty())
+    {
+        printf("stack 1 | 2 top: %d | %d \n", stack1.top(), stack2.top());
+        if (stack1.top() != stack2.top())
+            return false;
+        
+        stack1.pop();
+        stack2.pop();
+    }
+    
+    
+
+    
+    
+    return true;
 }
 
 int main(int argc, char** argv)
 {
-    char* input = argv[1];
-    printf("input lenght %ld \n", strlen(input));
-    auto time1 = std::chrono::high_resolution_clock::now();
-    auto time2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> elapse = time2 - time1;
+    std::string input;
+    printf("input: ");
+    std::getline(std::cin, input);
+    bool correct = IsPalindrome(input);
+    //printf("input lenght %ld \n", strlen(input));
+    if (correct)
+        printf("%s is a palindrome! \n", input.c_str());
+    else
+        printf("%s is not a palindrome! \n", input.c_str());
+
+    return 1;
+
 }
